2e080642a396cf1b32cc7d1947ba46a8ff188809
comzyh <comzyh@gmail.com>
2019-07-12 02:01:52 +0800

    Add WPF device scanner

---
+++ WpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace WpfDemo
+{
+    public class DeviceInfo : INotifyPropertyChanged
+    {
+        public DeviceInfo(DeviceInformation deviceInfoIn)
+        {
+            DeviceInformation = deviceInfoIn;
+        }
+
+        public DeviceInformation DeviceInformation { get; private set; }
+
+        public string DeviceId => DeviceInformation.Id;
+        public string DeviceName => DeviceInformation.Name;
+
+        public string DeviceMAC => (string)DeviceInformation.Properties["System.Devices.Aep.DeviceAddress"];
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Update(DeviceInformationUpdate deviceInfoUpdate)
+        {
+            DeviceInformation.Update(deviceInfoUpdate);
+            OnPropertyChanged("Name");
+            OnPropertyChanged("DeviceInformation");
+        }
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DeviceWatcher deviceWatcher;
+
+        public ObservableCollection<DeviceInfo> DeviceCollection { get; set; }
+        public MainWindow()
+        {
+            DataContext = this;
+            DeviceCollection = new ObservableCollection<DeviceInfo>();
+            InitializeComponent();
+        }
+
+        public void StartScan()
+        {
+            if (deviceWatcher == null)
+            {
+                string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected", "System.Devices.Aep.Bluetooth.Le.IsConnectable" };
+
+                // BT_Code: Example showing paired and non-paired in a single query.
+                string aqsAllBluetoothLEDevices = "(System.Devices.Aep.ProtocolId:=\"{bb7bb05e-5972-42b5-94fc-76eaa7084d49}\")";
+
+                BluetoothLEDevice.GetDeviceSelectorFromPairingState(false);
+
+                deviceWatcher =
+                        DeviceInformation.CreateWatcher(
+                            aqsAllBluetoothLEDevices,
+                            requestedProperties,
+                            DeviceInformationKind.AssociationEndpoint);
+                deviceWatcher.Added += DeviceWatcher_Added;
+                deviceWatcher.Updated += DeviceWatcher_Updated;
+                deviceWatcher.Removed += DeviceWatcher_Removed;
+                deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+                deviceWatcher.Stopped += DeviceWatcher_Stopped;
+                deviceWatcher.Start();
+
+            }
+        }
+        public void StopScan()
+        {
+            if (deviceWatcher != null)
+            {
+                // Unregister the event handlers.
+                deviceWatcher.Added -= DeviceWatcher_Added;
+                deviceWatcher.Updated -= DeviceWatcher_Updated;
+                deviceWatcher.Removed -= DeviceWatcher_Removed;
+                deviceWatcher.EnumerationCompleted -= DeviceWatcher_EnumerationCompleted;
+                deviceWatcher.Stopped -= DeviceWatcher_Stopped;
+
+                // Stop the watcher.
+                deviceWatcher.Stop();
+                deviceWatcher = null;
+            }
+        }
+        private void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation deviceInfo)
+        {
+            Debug.WriteLine(String.Format("Added {0}{1}", deviceInfo.Id, deviceInfo.Name));
+            if (sender != deviceWatcher) { return; }
+            Dispatcher.Invoke(() =>
+            {
+                DeviceCollection.Add(new DeviceInfo(deviceInfo));
+            });
+        }
+        private void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate deviceInfoUpdate)
+        {
+            if (sender != deviceWatcher) { return; }
+            DeviceInfo dev = FindDeviceInfoById(deviceInfoUpdate.Id);
+            if (dev == null) { return; }
+            dev.Update(deviceInfoUpdate);
+        }
+        private void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate deviceInfoUpdate)
+        {
+            string id = deviceInfoUpdate.Id;
+            Dispatcher.Invoke(() =>
+            {
+                DeviceInfo dev  = FindDeviceInfoById(deviceInfoUpdate.Id);
+                if (dev != null)
+                {
+                    DeviceCollection.Remove(dev);
+                }
+                
+            });
+        }
+        private void DeviceWatcher_EnumerationCompleted(DeviceWatcher sender, object e)
+        {
+
+        }
+        private void DeviceWatcher_Stopped(DeviceWatcher sender, object e)
+        {
+
+        }
+
+        private DeviceInfo FindDeviceInfoById(string id)
+        {
+            foreach (DeviceInfo dev in DeviceCollection)
+            {
+                if (dev.DeviceId == id)
+                {
+                    return dev;
+                }
+            }
+            return null;
+        }
+
+        #region "UICODE"
+        private void btnStartScanClick(object sender, RoutedEventArgs e)
+        {
+            StartScan();
+        }
+        #endregion
+
+    }
+}
